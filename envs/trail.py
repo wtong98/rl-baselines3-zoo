95a39c30bd96893cd33bc962f7e524c1608990e6
wtong98 <williamtong105@gmail.com>
2021-10-01 23:05:52 -0400

    Lay skeleton for trail env

---
+++ envs/trail.py
@@ -0,0 +1,30 @@
+import gym
+from gym import spaces
+
+
+class TrailEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        super(TrailEnv, self).__init__()
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
+        # Example for using image as input (channel-first; channel-last also works):
+        self.observation_space = spaces.Box(low=0, high=255,
+                                            shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8)
+
+    def step(self, action):
+        ...
+        return observation, reward, done, info
+
+    def reset(self):
+        ...
+        return observation  # reward, done, info can't be included
+
+    def render(self, mode='human'):
+        ...
+
+    def close(self):
+        ...
